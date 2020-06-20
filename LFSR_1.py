5893dcb2d521a204b4b64448cf43281ae3bdfd74
sinalpha <hanhdhx65@gmail.com>
2020-06-20 19:45:10 +0900

    LFSR 알고리즘

---
+++ LFSR_1.py
@@ -0,0 +1,15 @@
+list_ = [1, 1, 1, 0, 0, 1]
+list_1 = []
+for a in range(0, 16):
+
+    sum1 = list_[5] + list_[0] + list_[4]
+    list_1.append(list_[5])
+    for b in [4, 3, 2, 1, 0]:
+        list_[b+1] = list_[b]
+
+    if sum1 % 2 == 1:
+        list_[0] = 1
+    else:
+        list_[0] = 0
+
+print(list_1)
